112fa1df3696f964aee4b2872460c058dc03a3e7
ErickAvlia23 <2511482019@mail.utec.edu.sv>
2023-04-19 22:43:42 -0600

    Init2

--- AvilaCamposParcial/app/src/main/java/sv/edu/utec/avilacamposparcial/MainActivity.java
+++ AvilaCamposParcial/app/src/main/java/sv/edu/utec/avilacamposparcial/MainActivity.java
@@ -3,11 +3,13 @@ package sv.edu.utec.avilacamposparcial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import sv.edu.utec.avilacamposparcial.Helper.AdminSQLiteOpenHelper;
 
@@ -43,6 +45,77 @@ Button insert, busca, actua, delete;
                 info.put("Apellidos", ape);
                 info.put("Telefono", tel);
                 info.put("Correo", cor);
+
+                bd.insert("Contactos", null, info);
+                bd.close();
+
+                Toast.makeText(getApplicationContext(), "Se insertaron los datos", Toast.LENGTH_LONG).show();
+            }
+        });
+        busca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(getApplicationContext(), "Parcial", null, 1);
+                SQLiteDatabase bd=admin.getWritableDatabase();
+                String tel=telefono.getText().toString();
+
+                Cursor filas=bd.rawQuery("SELECT Nombre,Apellidos,Correo FROM Contactos WHERE Telefono="+tel, null);
+
+                if(filas.moveToFirst()){
+                    nombre.setText(filas.getString(0));
+                    apellido.setText(filas.getString(1));
+                    correo.setText(filas.getString(2));
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "No se encontro el registro", Toast.LENGTH_LONG).show();
+                }
+                bd.close();
+            }
+        });
+        actua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(getApplicationContext(), "Parcial", null, 1);
+                SQLiteDatabase bd=admin.getWritableDatabase();
+                String nom=nombre.getText().toString();
+                String ape=apellido.getText().toString();
+                String tel=telefono.getText().toString();
+                String cor=correo.getText().toString();
+
+                ContentValues info=new ContentValues();
+                info.put("Nombre", nom);
+                info.put("Apellidos", ape);
+                info.put("Telefono", tel);
+                info.put("Correo", cor);
+
+                int cat=bd.update("Contactos", info, "Telefono="+tel, null);
+                bd.close();
+                if(cat==1){
+                    Toast.makeText(getApplicationContext(), "Se modifico el registro", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "No se modifico el registro", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(getApplicationContext(), "Parcial", null, 1);
+                SQLiteDatabase bd=admin.getWritableDatabase();
+                String tel=telefono.getText().toString();
+
+                int cat=bd.delete("Contactos", "Telefono="+tel, null);
+                bd.close();
+                nombre.setText("");
+                apellido.setText("");
+                correo.setText("");
+                if(cat==1){
+                    Toast.makeText(getApplicationContext(), "Se borro el registro", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "No se borro el registro", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
